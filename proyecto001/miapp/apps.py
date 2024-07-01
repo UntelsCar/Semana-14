a36d475922d06861ab11e2041a747625ca7c4e99
Carlos P~ <2123110286@untels.edu.pe>
2024-06-30 18:19:29 -1200

    parte 3

--- proyecto001/miapp/apps.py
+++ proyecto001/miapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MiappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'miapp'
+    verbose_name = 'Revista UNTELS'
--- proyecto001/miapp/models.py
+++ proyecto001/miapp/models.py
@@ -2,18 +2,31 @@ from django.db import models
 
 # Create your models here.
 class Articulo(models.Model):
-    titulo = models.CharField(max_length=150)
-    contenido = models.TextField()
-    imagen = models.ImageField(default='null')
-    publicado = models.BooleanField()
-    # auto_now_add me permitirá registrar 
-    # la fecha cuando cree el registro
-    creado = models.DateTimeField(auto_now_add=True)
-    # auto_now me permitirá registrar 
+    titulo = models.CharField(max_length=150, verbose_name="Título")
+    contenido = models.TextField(verbose_name="Contenido")
+    imagen = models.ImageField(default='null', verbose_name="Miniatura")
+    publicado = models.BooleanField(verbose_name="¿Publicado?")
+    # auto_now_add=True me pertmite 
+    # registrar la fecha de creación del registro
+    creado = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    # auto_now=True me permite registrar
     # la fecha cuando se modifique el registro
-    actualizado = models.DateTimeField(auto_now=True)
+    actualizado = models.DateTimeField(auto_now=True, verbose_name="Editado")
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"
+        ordering = ['titulo']        
+    def __str__(self):
+        if self.publicado:
+                publicado = "Publicado"
+        else:
+                publicado = "No Publicado / En Revisión"
+        return f"{self.titulo}. Creado el:{self.creado}. Publicado: {self.publicado}"
 class Categoria(models.Model):
     nombre = models.CharField(max_length=110)
     descripcion = models.CharField(max_length=250)
     # DateField() para guardar la fecha manualmente
     creado = models.DateField()
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
